b8d2488250f71928fd0b216b5b7f0639980ad11a
SSlavyanin <156539470+SSlavyanin@users.noreply.github.com>
2025-04-19 00:16:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Tools Bot is working!"
+
+# Добавляем прослушивание порта
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
 
