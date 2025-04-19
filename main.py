9fc5c7824212fa33052b938b1c94258583cffd55
SSlavyanin <156539470+SSlavyanin@users.noreply.github.com>
2025-04-19 14:37:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -101,4 +101,5 @@ def create_tool():
     return send_file(zip_buffer, as_attachment=True, download_name=f"{task}.zip")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
